a99b3c4b19bf21a55cafc56d0595c36b13bca6de
hannan9560 <ruhul4410@gmail.com>
2017-03-08 19:32:33 +0600

    First

---
+++ OOP.java
@@ -0,0 +1,15 @@
+
+public class OOP {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st1 = new Student(1, "Hannan", 60.0, 70.0, 80.0);
+		Student st2 = new Student(2, "Sadhan", 80.0, 70.0, 90.0);
+		System.out.println(st1);
+		System.out.println(st2);
+	}
+
+}
